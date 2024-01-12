01b70bd0bfe66eba47be9e08a44d2788e1c65f36
abersheeran <me@abersheeran.com>
2024-01-12 23:13:27 +0800

    -

--- main/application.py
+++ main/application.py
@@ -1,3 +1,6 @@
+import asyncio
+
+import httpx
 from kui.asgi import Kui
 
 from .ai_api.gemini import initial_gemini_config
@@ -22,3 +25,62 @@ async def initial_cache(app: Kui) -> None:
     app.state.picture_cache = {}
     app.state.pending_queue = {}
     app.state.pending_queue_count = {}
+
+
+@app.on_startup
+async def initial_token(app: Kui) -> None:
+    async with httpx.AsyncClient() as client:
+        resp = await client.get(
+            "https://api.weixin.qq.com/cgi-bin/token",
+            params={
+                "grant_type": "client_credential",
+                "appid": settings.app_id,
+                "secret": settings.app_secret,
+            },
+        )
+        resp.raise_for_status()
+        data = resp.json()
+        app.state.access_token = data["access_token"]
+        expires_in = data["expires_in"]
+
+    task = asyncio.create_task(asyncio.sleep(expires_in - 60))
+    task.add_done_callback(lambda future: asyncio.create_task(initial_token(app)))
+
+
+@app.on_startup
+async def initial_menus(app: Kui) -> None:
+    menus = {
+        "button": [
+            {"name": "拍照上传", "type": "pic_photo_or_album", "key": "pzsc"},
+            {
+                "name": "发图",
+                "sub_button": [
+                    {
+                        "type": "pic_sysphoto",
+                        "name": "系统拍照发图",
+                        "key": "rselfmenu_1_0",
+                        "sub_button": [],
+                    },
+                    {
+                        "type": "pic_photo_or_album",
+                        "name": "拍照或者相册发图",
+                        "key": "rselfmenu_1_1",
+                        "sub_button": [],
+                    },
+                    {
+                        "type": "pic_weixin",
+                        "name": "微信相册发图",
+                        "key": "rselfmenu_1_2",
+                        "sub_button": [],
+                    },
+                ],
+            },
+        ]
+    }
+    async with httpx.AsyncClient() as client:
+        resp = await client.post(
+            "https://api.weixin.qq.com/cgi-bin/menu/create",
+            params={"access_token": app.state.access_token},
+            json=menus,
+        )
+        resp.raise_for_status()
